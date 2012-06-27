324ff81ea9d4c471a517b528f070ca8e13eea884
Chris Bosco <chris@aviary.com>
2012-06-27 12:45:38 -0400

    Create miniColors core library and separate functions into lib / input element concerns. Still lots of coupling to input element

--- jquery.miniColors.js
+++ jquery.miniColors.js
@@ -6,11 +6,16 @@
  * Dual licensed under the MIT or GPL Version 2 licenses
  *
 */
-if(jQuery) (function($) {
-	
+if(jQuery && jQuery.miniColors) (function($) {
+
 	$.extend($.fn, {
 		
 		miniColors: function(o, data) {
+
+			//
+			// Creates a new model instance
+			//
+			var mc = new $.miniColors();
 			
 			var create = function(input, o, data) {
 				//
@@ -18,9 +23,9 @@ if(jQuery) (function($) {
 				//
 				
 				// Determine initial color (defaults to white)
-				var color = expandHex(input.val());
+				var color = mc.expandHex(input.val());
 				if( !color ) color = 'ffffff';
-				var hsb = hex2hsb(color);
+				var hsb = mc.hex2hsb(color);
 				
 				// Create trigger
 				var trigger = $('<a class="miniColors-trigger" style="background-color: #' + color + '" href="#"></a>');
@@ -59,7 +64,7 @@ if(jQuery) (function($) {
 				
 				// Hide on blur
 				input.bind('blur.miniColors', function(event) {
-					var hex = expandHex(input.val());
+					var hex = mc.expandHex(input.val());
 					input.val( hex ? '#' + convertCase(hex, input.data('letterCase')) : '' );
 				});
 				
@@ -70,14 +75,14 @@ if(jQuery) (function($) {
 				
 				// Update when color is typed in
 				input.bind('keyup.miniColors', function(event) {
-					setColorFromInput(input);
+					mc.setColorFromInput(input);
 				});
 				
 				// Handle pasting
 				input.bind('paste.miniColors', function(event) {
 					// Short pause to wait for paste to complete
 					setTimeout( function() {
-						setColorFromInput(input);
+						mc.setColorFromInput(input);
 					}, 5);
 				});
 				
@@ -133,85 +138,32 @@ if(jQuery) (function($) {
 				hide();				
 				
 				// Generate the selector
-				var selector = $('<div class="miniColors-selector"></div>');
+				var selector = mc.buildSelector(input);
+				// position selector with respect to input
 				selector
-					.append('<div class="miniColors-colors" style="background-color: #FFF;"><div class="miniColors-colorPicker"></div></div>')
-					.append('<div class="miniColors-hues"><div class="miniColors-huePicker"></div></div>')
 					.css({
 						top: input.is(':visible') ? input.offset().top + input.outerHeight() : input.data('trigger').offset().top + input.data('trigger').outerHeight(),
-						left: input.is(':visible') ? input.offset().left : input.data('trigger').offset().left,
-						display: 'none'
+						left: input.is(':visible') ? input.offset().left : input.data('trigger').offset().left
 					})
 					.addClass( input.attr('class') );
-				
-				// Set background for colors
-				var hsb = input.data('hsb');
-				selector
-					.find('.miniColors-colors')
-					.css('backgroundColor', '#' + hsb2hex({ h: hsb.h, s: 100, b: 100 }));
-				
-				// Set colorPicker position
-				var colorPosition = input.data('colorPosition');
-				if( !colorPosition ) colorPosition = getColorPositionFromHSB(hsb);
-				selector.find('.miniColors-colorPicker')
-					.css('top', colorPosition.y + 'px')
-					.css('left', colorPosition.x + 'px');
-				
-				// Set huePicker position
-				var huePosition = input.data('huePosition');
-				if( !huePosition ) huePosition = getHuePositionFromHSB(hsb);
-				selector.find('.miniColors-huePicker').css('top', huePosition.y + 'px');
-				
+
 				// Set input data
 				input
 					.data('selector', selector)
 					.data('huePicker', selector.find('.miniColors-huePicker'))
 					.data('colorPicker', selector.find('.miniColors-colorPicker'))
-					.data('mousebutton', 0);
+					.data('mousebutton',0)
+					.on('updateInput', function(event, data) {
+						input.val( '#' + convertCase(data.hex, input.data('letterCase')) );
+					});
 					
 				$('BODY').append(selector);
 				selector.fadeIn(100);
 				
-				// Prevent text selection in IE
-				selector.bind('selectstart', function() { return false; });
-				
-				$(document).bind('mousedown.miniColors touchstart.miniColors', function(event) {
-					
-					input.data('mousebutton', 1);
-					
-					if( $(event.target).parents().andSelf().hasClass('miniColors-colors') ) {
-						event.preventDefault();
-						input.data('moving', 'colors');
-						moveColor(input, event);
-					}
-					
-					if( $(event.target).parents().andSelf().hasClass('miniColors-hues') ) {
-						event.preventDefault();
-						input.data('moving', 'hues');
-						moveHue(input, event);
-					}
-					
-					if( $(event.target).parents().andSelf().hasClass('miniColors-selector') ) {
-						event.preventDefault();
-						return;
-					}
-					
-					if( $(event.target).parents().andSelf().hasClass('miniColors') ) return;
-					
-					hide(input);
-				});
-				
-				$(document)
-					.bind('mouseup.miniColors touchend.miniColors', function(event) {
-					    event.preventDefault();
-						input.data('mousebutton', 0).removeData('moving');
-					})
-					.bind('mousemove.miniColors touchmove.miniColors', function(event) {
-						event.preventDefault();
-						if( input.data('mousebutton') === 1 ) {
-							if( input.data('moving') === 'colors' ) moveColor(input, event);
-							if( input.data('moving') === 'hues' ) moveHue(input, event);
-						}
+				mc.bindSelectorEvents(input);
+
+				$(mc).on('clickOutsideSelector', function(event) {
+						hide(input);
 					});
 				
 			};
@@ -233,134 +185,7 @@ if(jQuery) (function($) {
 					});
 				});
 				
-				$(document).unbind('.miniColors');
-				
-			};
-			
-			var moveColor = function(input, event) {
-
-				var colorPicker = input.data('colorPicker');
-				
-				colorPicker.hide();
-				
-				var position = {
-					x: event.pageX,
-					y: event.pageY
-				};
-				
-				// Touch support
-				if( event.originalEvent.changedTouches ) {
-					position.x = event.originalEvent.changedTouches[0].pageX;
-					position.y = event.originalEvent.changedTouches[0].pageY;
-				}
-				position.x = position.x - input.data('selector').find('.miniColors-colors').offset().left - 5;
-				position.y = position.y - input.data('selector').find('.miniColors-colors').offset().top - 5;
-				if( position.x <= -5 ) position.x = -5;
-				if( position.x >= 144 ) position.x = 144;
-				if( position.y <= -5 ) position.y = -5;
-				if( position.y >= 144 ) position.y = 144;
-				
-				input.data('colorPosition', position);
-				colorPicker.css('left', position.x).css('top', position.y).show();
-				
-				// Calculate saturation
-				var s = Math.round((position.x + 5) * 0.67);
-				if( s < 0 ) s = 0;
-				if( s > 100 ) s = 100;
-				
-				// Calculate brightness
-				var b = 100 - Math.round((position.y + 5) * 0.67);
-				if( b < 0 ) b = 0;
-				if( b > 100 ) b = 100;
-				
-				// Update HSB values
-				var hsb = input.data('hsb');
-				hsb.s = s;
-				hsb.b = b;
-				
-				// Set color
-				setColor(input, hsb, true);
-			};
-			
-			var moveHue = function(input, event) {
-				
-				var huePicker = input.data('huePicker');
-				
-				huePicker.hide();
-				
-				var position = {
-					y: event.pageY
-				};
-				
-				// Touch support
-				if( event.originalEvent.changedTouches ) {
-					position.y = event.originalEvent.changedTouches[0].pageY;
-				}
-				
-				position.y = position.y - input.data('selector').find('.miniColors-colors').offset().top - 1;
-				if( position.y <= -1 ) position.y = -1;
-				if( position.y >= 149 ) position.y = 149;
-				input.data('huePosition', position);
-				huePicker.css('top', position.y).show();
-				
-				// Calculate hue
-				var h = Math.round((150 - position.y - 1) * 2.4);
-				if( h < 0 ) h = 0;
-				if( h > 360 ) h = 360;
-				
-				// Update HSB values
-				var hsb = input.data('hsb');
-				hsb.h = h;
-				
-				// Set color
-				setColor(input, hsb, true);
-				
-			};
-			
-			var setColor = function(input, hsb, updateInput) {
-				input.data('hsb', hsb);
-				var hex = hsb2hex(hsb);	
-				if( updateInput ) input.val( '#' + convertCase(hex, input.data('letterCase')) );
-				input.data('trigger').css('backgroundColor', '#' + hex);
-				if( input.data('selector') ) input.data('selector').find('.miniColors-colors').css('backgroundColor', '#' + hsb2hex({ h: hsb.h, s: 100, b: 100 }));
-				
-				// Fire change callback
-				if( input.data('change') ) {
-					if( hex === input.data('lastChange') ) return;
-					input.data('change').call(input.get(0), '#' + hex, hsb2rgb(hsb));
-					input.data('lastChange', hex);
-				}
-				
-			};
-			
-			var setColorFromInput = function(input) {
-				
-				input.val('#' + cleanHex(input.val()));
-				var hex = expandHex(input.val());
-				if( !hex ) return false;
-				
-				// Get HSB equivalent
-				var hsb = hex2hsb(hex);
-				
-				// If color is the same, no change required
-				var currentHSB = input.data('hsb');
-				if( hsb.h === currentHSB.h && hsb.s === currentHSB.s && hsb.b === currentHSB.b ) return true;
-				
-				// Set colorPicker position
-				var colorPosition = getColorPositionFromHSB(hsb);
-				var colorPicker = $(input.data('colorPicker'));
-				colorPicker.css('top', colorPosition.y + 'px').css('left', colorPosition.x + 'px');
-				input.data('colorPosition', colorPosition);
-				
-				// Set huePosition position
-				var huePosition = getHuePositionFromHSB(hsb);
-				var huePicker = $(input.data('huePicker'));
-				huePicker.css('top', huePosition.y + 'px');
-				input.data('huePosition', huePosition);
-				
-				setColor(input, hsb);
-				
-				return true;
+				mc.unBindSelectorEvents();
 				
 			};
 			
@@ -369,122 +194,6 @@ if(jQuery) (function($) {
 				if( letterCase === 'uppercase' ) return string.toUpperCase();
 				return string;
 			};
-			
-			var getColorPositionFromHSB = function(hsb) {				
-				var x = Math.ceil(hsb.s / 0.67);
-				if( x < 0 ) x = 0;
-				if( x > 150 ) x = 150;
-				var y = 150 - Math.ceil(hsb.b / 0.67);
-				if( y < 0 ) y = 0;
-				if( y > 150 ) y = 150;
-				return { x: x - 5, y: y - 5 };
-			};
-			
-			var getHuePositionFromHSB = function(hsb) {
-				var y = 150 - (hsb.h / 2.4);
-				if( y < 0 ) h = 0;
-				if( y > 150 ) h = 150;				
-				return { y: y - 1 };
-			};
-			
-			var cleanHex = function(hex) {
-				return hex.replace(/[^A-F0-9]/ig, '');
-			};
-			
-			var expandHex = function(hex) {
-				hex = cleanHex(hex);
-				if( !hex ) return null;
-				if( hex.length === 3 ) hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-				return hex.length === 6 ? hex : null;
-			};			
-			
-			var hsb2rgb = function(hsb) {
-				var rgb = {};
-				var h = Math.round(hsb.h);
-				var s = Math.round(hsb.s*255/100);
-				var v = Math.round(hsb.b*255/100);
-				if(s === 0) {
-					rgb.r = rgb.g = rgb.b = v;
-				} else {
-					var t1 = v;
-					var t2 = (255 - s) * v / 255;
-					var t3 = (t1 - t2) * (h % 60) / 60;
-					if( h === 360 ) h = 0;
-					if( h < 60 ) { rgb.r = t1; rgb.b = t2; rgb.g = t2 + t3; }
-					else if( h < 120 ) {rgb.g = t1; rgb.b = t2; rgb.r = t1 - t3; }
-					else if( h < 180 ) {rgb.g = t1; rgb.r = t2; rgb.b = t2 + t3; }
-					else if( h < 240 ) {rgb.b = t1; rgb.r = t2; rgb.g = t1 - t3; }
-					else if( h < 300 ) {rgb.b = t1; rgb.g = t2; rgb.r = t2 + t3; }
-					else if( h < 360 ) {rgb.r = t1; rgb.g = t2; rgb.b = t1 - t3; }
-					else { rgb.r = 0; rgb.g = 0; rgb.b = 0; }
-				}
-				return {
-					r: Math.round(rgb.r),
-					g: Math.round(rgb.g),
-					b: Math.round(rgb.b)
-				};
-			};
-			
-			var rgb2hex = function(rgb) {
-				var hex = [
-					rgb.r.toString(16),
-					rgb.g.toString(16),
-					rgb.b.toString(16)
-				];
-				$.each(hex, function(nr, val) {
-					if (val.length === 1) hex[nr] = '0' + val;
-				});
-				return hex.join('');
-			};
-			
-			var hex2rgb = function(hex) {
-				hex = parseInt(((hex.indexOf('#') > -1) ? hex.substring(1) : hex), 16);
-				
-				return {
-					r: hex >> 16,
-					g: (hex & 0x00FF00) >> 8,
-					b: (hex & 0x0000FF)
-				};
-			};
-			
-			var rgb2hsb = function(rgb) {
-				var hsb = { h: 0, s: 0, b: 0 };
-				var min = Math.min(rgb.r, rgb.g, rgb.b);
-				var max = Math.max(rgb.r, rgb.g, rgb.b);
-				var delta = max - min;
-				hsb.b = max;
-				hsb.s = max !== 0 ? 255 * delta / max : 0;
-				if( hsb.s !== 0 ) {
-					if( rgb.r === max ) {
-						hsb.h = (rgb.g - rgb.b) / delta;
-					} else if( rgb.g === max ) {
-						hsb.h = 2 + (rgb.b - rgb.r) / delta;
-					} else {
-						hsb.h = 4 + (rgb.r - rgb.g) / delta;
-					}
-				} else {
-					hsb.h = -1;
-				}
-				hsb.h *= 60;
-				if( hsb.h < 0 ) {
-					hsb.h += 360;
-				}
-				hsb.s *= 100/255;
-				hsb.b *= 100/255;
-				return hsb;
-			};			
-			
-			var hex2hsb = function(hex) {
-				var hsb = rgb2hsb(hex2rgb(hex));
-				// Zero out hue marker for black, white, and grays (saturation === 0)
-				if( hsb.s === 0 ) hsb.h = 360;
-				return hsb;
-			};
-			
-			var hsb2hex = function(hsb) {
-				return rgb2hex(hsb2rgb(hsb));
-			};
-
 			
 			// Handle calls to $([selector]).miniColors()
 			switch(o) {
@@ -517,7 +226,7 @@ if(jQuery) (function($) {
 					if( data === undefined ) {
 						if( !$(this).hasClass('miniColors') ) return;
 						var input = $(this),
-							hex = expandHex(input.val());
+							hex = mc.expandHex(input.val());
 						return hex ? '#' + convertCase(hex, input.data('letterCase')) : null;
 					}
 					
@@ -525,7 +234,7 @@ if(jQuery) (function($) {
 					$(this).each( function() {
 						if( !$(this).hasClass('miniColors') ) return;
 						$(this).val(data);
-						setColorFromInput($(this));
+						mc.setColorFromInput($(this));
 					});
 					
 					return $(this);
