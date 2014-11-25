b041bca40fe2254a2ca54ccc724c9c3b0648e9f5
Cory LaViska <cory@abeautifulsite.net>
2014-11-25 12:33:09 -0500

    Tell JSHint to ignore bitwise operators

--- jquery.minicolors.js
+++ jquery.minicolors.js
@@ -755,9 +755,11 @@ if(jQuery) (function($) {
     function hex2rgb(hex) {
         hex = parseInt(((hex.indexOf('#') > -1) ? hex.substring(1) : hex), 16);
         return {
+            /* jshint ignore:start */
             r: hex >> 16,
             g: (hex & 0x00FF00) >> 8,
             b: (hex & 0x0000FF)
+            /* jshint ignore:end */
         };
     }
 
