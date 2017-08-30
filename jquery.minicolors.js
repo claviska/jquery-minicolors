106c1988adc488a8b0aba4adb2a61e90de2abaa0
kyle-jennings <kjennings@gmail.com>
2017-08-30 17:27:57 -0400

    updated the events handler

--- jquery.minicolors.js
+++ jquery.minicolors.js
@@ -975,7 +975,7 @@
   }
 
   // Handle events
-  $([document, top.document])
+  $([document])
     // Hide on clicks outside of the control
     .on('mousedown.minicolors touchstart.minicolors', function(event) {
       if(!$(event.target).parents().add(event.target).hasClass('minicolors')) {
